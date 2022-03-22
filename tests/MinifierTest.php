a481e9352bf9dac299fad1cdc302730129d92cb1
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2022-03-22 01:45:26 +0100

    Test the file minifier.

---
+++ tests/MinifierTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Jaxon\Utils\Tests;
+
+use Jaxon\Utils\File\FileMinifier;
+use PHPUnit\Framework\TestCase;
+
+use function file_exists;
+use function filesize;
+
+/**
+ * @covers FileMinifier
+ */
+final class MinifierTest extends TestCase
+{
+    public function testFileError()
+    {
+        $sSrcFile = __DIR__ . '/minifier/nosrc.js';
+        $sSrcMinFile = __DIR__ . '/minifier/src.min.js';
+        $sDstMinFile = __DIR__ . '/minifier/dst.min.js';
+        $xMinifier = new FileMinifier();
+        $bResult = $xMinifier->minify($sSrcFile, $sDstMinFile);
+
+        $this->assertFalse($bResult);
+    }
+
+    public function testMinifier()
+    {
+        $sSrcFile = __DIR__ . '/minifier/src.js';
+        $sSrcMinFile = __DIR__ . '/minifier/src.min.js';
+        $sDstMinFile = __DIR__ . '/minifier/dst.min.js';
+        $xMinifier = new FileMinifier();
+        $bResult = $xMinifier->minify($sSrcFile, $sDstMinFile);
+
+        $this->assertTrue($bResult);
+        $this->assertTrue(file_exists($sDstMinFile));
+        $this->assertEquals(filesize($sSrcMinFile), filesize($sDstMinFile));
+    }
+}
